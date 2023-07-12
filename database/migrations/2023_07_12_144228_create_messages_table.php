93d9f03c8fd7b72a98bbcb61e647752852c30aa2
andreaconv <andrea2convertini@gmail.com>
2023-07-12 16:56:47 +0200

    create migration messages

---
+++ database/migrations/2023_07_12_144228_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('object');
+            $table->text('text');
+            $table->string('author_email');
+            $table->boolean('message_read')->default(0);
+            $table->string('author_first_name');
+            $table->string('author_last_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
