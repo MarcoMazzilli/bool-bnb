a392d74501491d33e6fcbfad66ce3dd47f6e142e
carloDM <carlo.de.mauro.work@gmail.com>
2023-07-12 17:22:47 +0200

    [guest home] basic header component & scss files

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,27 @@
 <script>
+import {store} from'../data/store.js';
+import Header from '../views/guest/components/header.vue';
 export default {
-    name: 'home'
+    name: 'home',
 
+    components:{Header},
+    methods:{},
+    computed:{},
+    mounted(){}
 }
 </script>
 
 <template>
-<h1>Hello world</h1>
+  <div class="main_wrapper">
+    <Header/>
+    <div class="container">
+
+      <h1>home guest</h1>
+    </div>
+
+  </div>
 </template>
 
-<style lang="scss" scoped>
-
+<style lang="scss" >
+@import '../scss/mainGuest.scss'
 </style>
