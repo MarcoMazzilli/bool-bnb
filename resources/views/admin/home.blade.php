756f5f8c0a0da8c49fb30c489c7861b113202ffe
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-07-15 19:47:24 +0200

    [example] dashboard home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,25 +4,47 @@
 
 <div class="container">
 
-  <h2 class="fs-4 text-secondary my-4">{{ __('Dashboard') }}</h2>
+    <h2 class="text-secondary my-4">Benvenuto {{ Auth::user()->name }} {{ Auth::user()->last_name }} </h2>
 
-  <p>{{ Auth::user()->name }} utente registrato il {{ Auth::user()->created_at }}</p>
-
-  {{-- -------------------- dash nativa  --}}
-
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ Auth::user()->name }} {{ __('Dashboard') }}</div>
+    <div class="row justify-content-between">
+        <div class="col col-7">
+            <div class="card shadow">
+                <div class="card-header">Recap</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                    <div>
+                        <span class="title fw-bold">Tot. appartamenti :</span><span class="result"> Da inserire la variabile ricevuta tramite una query </span> <hr>
+                    </div>
+                    <div>
+                        <span class="title fw-bold">Tot. appartamenti Sponsorizzati :</span><span class="result"> Da inserire la variabile </span> <hr>
+                    </div>
+                    <div>
+                        <span class="title fw-bold">Tot. appartamenti visibili :</span><span class="result"> Da inserire la variabile </span> <hr>
+                    </div>
+                    <div>
+                        <span class="title fw-bold">Tot. appartamenti nascosti :</span><span class="result"> Da inserire la variabile </span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col col-3">
+            <div class="card shadow">
+                {{-- TODO: PASSARSI CON UNA QUERY TUTTI I MESSAGGI CON IL CAMPO READ FALSE --}}
+                <div class="card-header">N. nuovi messaggi</div>
+
+                <div class="card-body">
+
+                    <div>
+                        <p class="title fw-bold">Mittente :</p>
+                        <p class="result">anteprima messaggio</p>
+                    </div>
+                    <div>
+                        <p class="title fw-bold">Mittente :</p>
+                        <p class="result">anteprima messaggio</p>
+                    </div>
+
                 </div>
             </div>
         </div>
