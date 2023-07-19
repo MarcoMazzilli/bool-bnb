fe15d60a4aedd7537f30a64a3adae739a833a1f5
carloDM <carlo.de.mauro.work@gmail.com>
2023-07-19 14:37:43 +0200

    edit force autocomplete

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -268,11 +268,8 @@
 
     if (searchBoxContainer) {
         searchBoxContainer.appendChild(searchBoxHTML);
-        // console.log(ttSearchBox);
     }
     const inputElement = document.querySelector('.tt-search-box-input');
-    // console.log(inputElement);
-
     // attributi dell input
     Object.assign(inputElement, {
       id: 'address',
@@ -282,7 +279,67 @@
       placeholder: 'inserisci indirizzo',
       type: 'text'
     });
-    // console.log(inputElement);
+
+
+        // forzatura autocomplete
+        inputElement.addEventListener('blur', function() {verify(inputElement);});
+
+        function verify(inputElement) {
+            console.log('valore lasciato',inputElement.value);
+            const elements = document.querySelectorAll('.tt-search-box-result-list-bold');
+
+            let replaceAddres;
+
+            console.warn('first child',elements[2].firstChild.data);
+            console.warn('next silibing',elements[2].nextSibling);
+
+                if(elements[2].firstChild.data && elements[2].nextSibling ){
+                    replaceAddres = elements[2].firstChild.data + ' ' + elements[2].nextSibling.data;
+                    Object.assign(inputElement, {
+                    id: 'address',
+                    name: 'address',
+                    value: replaceAddres,
+                    className: 'form-control' + ' ' + 'tt-search-box-input',
+                    placeholder: 'Indirizzo appartamento',
+                    type: 'text'});
+
+                }else if(elements[2].firstChild.data){
+                    replaceAddres = elements[2].firstChild.data;
+                    Object.assign(inputElement, {
+                    id: 'address',
+                    name: 'address',
+                    value: replaceAddres,
+                    className: 'form-control' + ' ' + 'tt-search-box-input',
+                    placeholder: 'Indirizzo appartamento',
+                    type: 'text'});
+                }
+
+            console.warn(replaceAddres);
+            indirizzo = inputElement.value;
+            getCordianates();
+        };
+
+
+        // verifica dell indirizzo
+        const
+            TomtomBaseUrl ='https://api.tomtom.com/',
+            apiKey = "{{ env('API_TT_KEY') }}",
+            apiUrlSearchAddress = 'search/2/geocode/',
+            queryType = '.json?typeahead=false&limit=1&view=Unified&key=';
+        let indirizzo = 'inserisci indirizzo';
+
+        function convertAddress(address){
+            const converted = address.replace(/ /g,'%20') ;
+            console.log(converted);
+            return converted;
+        };
+        // stampa json link del geocoding
+        function   getCordianates(){
+            console.log(TomtomBaseUrl + apiUrlSearchAddress + convertAddress(indirizzo) + queryType + apiKey);
+        };
+
+
+// ---------------------------------------------------------------------------
 
 
 //Gestione anteprima immagine di copertina
