189c47e8ac45c18e043432e1880540cb52fe0899
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-07-19 11:12:23 +0200

    [Add] Client side register form validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,7 +21,7 @@
                                     <div class="col-md-6">
                                         <input id="name" type="text"
                                             class="form-control @error('name') is-invalid @enderror" name="name"
-                                            value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                            value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Inserisci il tuo nome">
 
                                         @error('name')
                                             <span class="invalid-feedback" role="alert">
@@ -38,7 +38,7 @@
                                     <div class="col-md-6">
                                         <input id="last_name" type="text"
                                             class="form-control @error('name') is-invalid @enderror" name="last_name"
-                                            value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                            value="{{ old('last_name') }}" required autocomplete="name" autofocus placeholder="Inserisci il tuo cognome">
 
                                         {{-- TODO: Gestire il messaggio di errore --}}
                                         @error('name')
@@ -56,7 +56,7 @@
                                     <div class="col-md-6">
                                         <input id="date_of_birth" type="date"
                                             class="form-control @error('name') is-invalid @enderror" name="date_of_birth"
-                                            value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                            value="{{ old('date_of_birth') }}" required autocomplete="name" autofocus>
 
                                         {{-- TODO: Gestire il messaggio di errore --}}
                                         @error('name')
@@ -74,7 +74,7 @@
                                     <div class="col-md-6">
                                         <input id="email" type="email"
                                             class="form-control @error('email') is-invalid @enderror" name="email"
-                                            value="{{ old('email') }}" required autocomplete="email">
+                                            value="{{ old('email') }}" required autocomplete="email" placeholder="Inserisci la tua mail">
 
                                         @error('email')
                                             <span class="invalid-feedback" role="alert">
@@ -91,7 +91,7 @@
                                     <div class="col-md-6">
                                         <input id="password" type="password"
                                             class="form-control @error('password') is-invalid @enderror" name="password"
-                                            required autocomplete="new-password">
+                                            required autocomplete="new-password" placeholder="Min 8 caratteri">
 
                                         @error('password')
                                             <span class="invalid-feedback" role="alert">
@@ -107,7 +107,7 @@
 
                                     <div class="col-md-6">
                                         <input id="password-confirm" type="password" class="form-control"
-                                            name="password_confirmation" required autocomplete="new-password">
+                                            name="password_confirmation" required autocomplete="new-password" placeholder="Conferma la tua password">
                                     </div>
                                 </div>
 
@@ -118,7 +118,7 @@
                                                 home</button></a>
 
                                         {{-- button register --}}
-                                        <button type="submit" class="btn">
+                                        <button disabled id="submit-btn"type="submit" class="btn">
                                             {{ __('Registrati') }}
                                         </button>
 
@@ -139,10 +139,11 @@
         const name = document.getElementById('name');
         const last_name = document.getElementById('last_name');
         const date_of_birth = document.getElementById('date_of_birth');
+        const submit_btn = document.getElementById('submit-btn')
 
         // FUNCTION
         function comparePassword(){
-            return(password.value === password_confirm.value)
+            return password.value === password_confirm.value
         }
 
         function pswdValidation(){
@@ -169,29 +170,15 @@
 
             specialChars.test(text) ? input.classList.add('is-invalid') : input.classList.add('is-valid')
             !specialChars.test(text) ? input.classList.remove('is-invalid') : input.classList.remove('is-valid')
+            return !specialChars.test(text)
         }
 
         function lenghtControl(text,input){
             text.length >= 3 ? input.classList.add('is-valid') : input.classList.add('is-invalid')
+            return text.length >= 3
         }
 
-        //EVENT
-        password_confirm.addEventListener('keyup', ()=>pswdValidation())
-
-        password.addEventListener('keyup', ()=>pswdValidation())
-
-        name.addEventListener('keyup',()=> {
-            errorSpecialCharacter(name.value, name);
-            lenghtControl(name.value, name);
-        })
-
-        last_name.addEventListener('keyup',()=> {
-            errorSpecialCharacter(last_name.value, last_name);
-            lenghtControl(last_name.value, last_name);
-        })
-
-        date_of_birth.addEventListener('change',()=>{
-
+        function ageCalculator(){
             const y_of_birth = new Date(date_of_birth.value)
 
             const now = new Date()
@@ -205,6 +192,61 @@
                 date_of_birth.classList.add('is-invalid')
             }
 
+            // console.log(current_year - y_of_birth.getFullYear() >= 18)
+            return current_year - y_of_birth.getFullYear() >= 18
+
+        }
+
+        function agreeSubmit(){
+            if(
+                comparePassword() &&
+                errorSpecialCharacter(name.value, name) &&
+                errorSpecialCharacter(last_name.value, last_name) &&
+                lenghtControl(name.value, name)&&
+                lenghtControl(last_name.value, last_name)&&
+                lenghtControl(password.value, name)&&
+                lenghtControl(password_confirm.value, last_name)&&
+                ageCalculator()
+                ){
+                    submit_btn.removeAttribute('disabled')
+
+                }else{
+                    submit_btn.setAttribute('disabled',true)
+                    console.log('comparePassword()',comparePassword())
+                    console.log('errorSpecialCharacter(name.value, name)',errorSpecialCharacter(name.value, name))
+                    console.log('errorSpecialCharacter(name.value, name)',errorSpecialCharacter(last_name.value, last_name))
+                    console.log('lenghtControl(name.value, name)',lenghtControl(name.value, name))
+                    console.log('lenghtControl(last_name.value, last_name)',lenghtControl(last_name.value, last_name))
+                    console.log('ageCalculator()',ageCalculator())
+                }
+        }
+
+        //EVENT
+        password_confirm.addEventListener('keyup', ()=>{
+            pswdValidation();
+            agreeSubmit();
+        })
+
+        password.addEventListener('keyup', ()=>{
+            pswdValidation();
+            agreeSubmit();
+        });
+
+        name.addEventListener('keyup',()=> {
+            errorSpecialCharacter(name.value, name);
+            lenghtControl(name.value, name);
+            agreeSubmit();
+        })
+
+        last_name.addEventListener('keyup',()=> {
+            errorSpecialCharacter(last_name.value, last_name);
+            lenghtControl(last_name.value, last_name);
+            agreeSubmit();
+        })
+
+        date_of_birth.addEventListener('change',()=>{
+            ageCalculator();
+            agreeSubmit();
         })
 
 
