80e0e86a8ce3749c480a8dd7344dcc4b0490862e
carloDM <carlo.de.mauro.work@gmail.com>
2023-07-21 13:07:54 +0200

    clean advanced search page

--- resources/js/pages/advanced-search.vue
+++ resources/js/pages/advanced-search.vue
@@ -7,33 +7,15 @@ export default {
     data(){
         return{
           store,
-          load: true,
-          selectedApartment: null,
         }
     },
+
     components:{ApartmentCard},
-    methods :{
-      // searchByRange(){
-      //   this.load = false;
-      //   let data = {
-      //     longitude : store.cord[0],
-      //     latitude : store.cord[1],
-      //     radius : 200,
-      //   }
-      //   axios.post('http://127.0.0.1:8000/api/find', data)
-      //   .then(result =>{
-      //     console.log('risultato ===>',result.data.filteredApartments);
-      //     store.apartmentsfiltred = result.data.filteredApartments;
-      //     this.load = true;
-      //   }).catch(error => {
-      //     console.log('Errori ===>',error)
-      //   })
-      // }
-    },
+
+    methods :{},
+
     mounted(){
         console.log('Advanced Search!');
-
-        // this.searchByRange();
     }
 }
 </script>
@@ -42,10 +24,14 @@ export default {
     <div class="AdvancedSearch_container" id="AdvancedSearch-page">
 
         <h1>Advanced Search</h1>
-        <!-- <button class="btn btn-danger" @click="searchByRange()">Console log</button> -->
+
 
         <div v-if="load" class="container py-5 d-flex flex-wrap justify-content-between">
-          <ApartmentCard v-for="apart in store.apartmentsfiltred" :key="apart.id" :apartmentData="apart" @apartmentSelected="showApartmentDetails" />
+
+          <ApartmentCard v-for="apart in store.apartmentsfiltred" :key="apart.id"
+          :apartmentData="apart"
+          />
+
         </div>
 
     </div>
