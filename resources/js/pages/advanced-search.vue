1c4275666ce2f4f83533c1fa322eb52e4caf39a3
carloDM <carlo.de.mauro.work@gmail.com>
2023-07-21 21:23:09 +0200

    advanced search bar fcss

--- resources/js/pages/advanced-search.vue
+++ resources/js/pages/advanced-search.vue
@@ -24,87 +24,112 @@ export default {
 <template>
     <div class="AdvancedSearch_container" id="AdvancedSearch-page">
 
-      <div id="search-type-selector">
-        <div class="container">
+      <div id="search-filter" class="container gx-0 debug2">
 
-          <span>Advanced Search</span>
+        <div id="search-type-selector" class="">
+            <div class="">
 
-          <span>Map Search</span>
+              <button class="src_typ_btn">
+                <span>Advanced Search</span>
+              </button>
 
+              <button class="src_typ_btn">
+                <span>Map Search</span>
+              </button>
+
+            </div>
         </div>
-      </div>
 
+        <div class="d-flex h-100 w-100">
 
-      <div id="search-filter">
+            <!-- search bar -->
+          <div class="advanced_search_bar mapping  d-flex flex-column  debug2">
 
-        <div class="advanced_search_bar d-flex justify-content-around  container debug2">
-
-          <!-- search bar -->
-          <div class="input-group flex-nowrap " id="" >
+            <div class="input-group flex-nowrap " id="" >
               <input @keypress.enter=""
               id="via" v-model="indirizzo"
               type="text"
               class="form-control"
               placeholder="Cerca indirizzo"
               aria-label="Username"
-              aria-describedby="addon-wrapping">
+              aria-describedby="">
 
-              <span @click=""
+              <!-- <span @click=""
               class="input-group-text search"
-              id="addon-wrapping"
+              id=""
               >
                 <i class="fa-solid fa-magnifying-glass"></i>
-              </span>
+              </span> -->
             </div>
+
+            <div id="mountMap" class="debug2">
+                <div class="map debug2" id="map" ref="mapRef"></div>
+            </div>
+
           <!-- search bar -->
-
-          <!-- stanze letti raggio -->
-          <div class="search_box d-flex flex-column justify-content-around align-items-center p-1  ">
-            <label class="form-label" for="rooms">numero di stanze</label>
-            <input class="form-control" id="rooms" type="number">
           </div>
 
-          <div class="search_box d-flex flex-column justify-content-around align-items-center p-1  ">
-            <label class="form-label" for="bad">numero di letti</label>
-            <input class="form-control" id="bad" type="number">
+          <!-- raggio metriquadri stanze letti bagno -->
+          <div class="advanced_search_bar option d-flex flex-column justify-content-around align-items-center  debug2">
+
+            <div class="search_box d-flex flex-column justify-content-around align-items-center p-1  ">
+              <label class="form-label" for="radius">raggio in km</label>
+              <input class="form-control" id="radius" type="number">
+            </div>
+
+            <div class="search_box d-flex flex-column justify-content-around align-items-center p-1  ">
+              <label class="form-label" for="bad">metriquadri</label>
+              <input class="form-control" id="bad" type="number">
+            </div>
+
+            <div class="search_box d-flex flex-column justify-content-around align-items-center p-1  ">
+              <label class="form-label" for="rooms">numero di stanze</label>
+              <input class="form-control" id="rooms" type="number">
+            </div>
+
+            <div class="search_box d-flex flex-column justify-content-around align-items-center p-1  ">
+              <label class="form-label" for="bad">numero di letti</label>
+              <input class="form-control" id="bad" type="number">
+            </div>
+
+            <div class="search_box d-flex flex-column justify-content-around align-items-center p-1  ">
+              <label class="form-label" for="bad">numero di bagni</label>
+              <input class="form-control" id="bad" type="number">
+            </div>
+
+
+
           </div>
+          <!-- raggio metriquadri stanze letti bagno -->
 
-          <div class="search_box d-flex flex-column justify-content-around align-items-center p-1  ">
-            <label class="form-label" for="radius">raggio in km</label>
-            <input class="form-control" id="radius" type="number">
-          </div>
-        </div>
-        <!-- stanze letti raggio -->
+          <!-- servizi -->
+          <div class="advanced_search_bar service  debug2">
 
-        <!-- servizi -->
-        <div class="advanced_search_bar  mt-1  container debug2">
-
-          <div class="btn_group " role="group" aria-label="Basic checkbox toggle button group">
+            <div class="btn_group " role="group" aria-label="Basic checkbox toggle button group">
 
             <div v-for="(service, index) in store.services" :key="index" style="display: inline-block;">
               <input type="checkbox" class="btn-check" :id="index" autocomplete="off">
               <label class="btn btn-outline-primary d-flex justify-content-center align-items-center" :for="index">   {{ service }}</label>
             </div>
+            </div>
+
           </div>
 
         </div>
 
       </div>
 
-      <div id="map-search"></div>
-
-
 
 
 
 <!-- ---------------result -->
-        <div v-if="load" class="container py-5 d-flex flex-wrap justify-content-between">
+      <div v-if="load" class="container py-5 d-flex flex-wrap justify-content-between">
 
           <ApartmentCard v-for="apart in store.apartmentsfiltred" :key="apart.id"
           :apartmentData="apart"
           />
 
-        </div>
+      </div>
 
     </div>
 </template>
@@ -112,20 +137,64 @@ export default {
 <style lang="scss" scoped>
 @import '../../scss/var';
 
-.advanced_search_bar{
-  // width: 80%;
-  margin: 0px auto;
-  // height: 60px;
 
-  &.address{
-    height: 40px;
+.src_typ_btn {
+	box-shadow:inset 0px 0px 0px 0px #ffffff;
+	background:linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
+	background-color:#f9f9f9;
+	border-radius:5px 5px 0px 0px;
+	border:1px solid #dcdcdc;
+	display:inline-block;
+	cursor:pointer;
+	color:#666666;
+	font-family:Arial;
+	font-size:12px;
+	font-weight:bold;
+	padding:2px 25px;
+
+  &:hover {
+    background:linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
+    background-color:#e9e9e9;
   }
+  &:active {
+    position:relative;
+    top:1px;
+  }
+}
+
+.advanced_search_bar{
+  margin: 0px auto;
+
+  &.mapping{
+    width: 78%;
+    margin-right: 1%;
+  }
+  &.option{
+    width: 10%;
+  }
+  &.service{
+    width: 12%;
+    height: 300px;
+    padding: 2px;
+    overflow: hidden;
+  }
+
+  #search-type-selector{
+    margin: 0 !important;
+    padding: 0 !important;
+  }
+
 
   & .search_box{
     width: calc(100% / 8);
     height: 100%;
+    margin: 2px 0px;
     min-width: 110px;
     max-width: 160px;
+    border-color: lighten($low-black, 80%);
+    box-shadow: inset 0px 0px 2px 1px lighten($brand-main, 20%);
+    background-color: lighten($brand-main, 25%);
+    border-radius: 7px;
 
 
     & input, label{
@@ -138,25 +207,31 @@ export default {
     & input{
       width: 80%;
       padding: 0px;
+      background-color: lighten($brand-main, 30%);
     }
   }
 }
-
+.input-group{
+  height: 30px;
+  max-width: 400px;
+  margin-right: 3px;
+}
 .btn_group{
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   width: 100%;
-  height: 190px;
-  overflow: auto;
+  height: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
   .btn{
     padding: 2px 10px;
-    margin: 2px 2px;
+    margin: 2px 2px 2px 2px;
     max-width: 80px;
     font-size: 0.8rem;
     min-width: 110px;
     // max-width: 160px;
-    min-height: 90px;
+    // min-height: 90px;
     // max-height: 160px;
 
   }
@@ -176,11 +251,18 @@ export default {
     }
   }
 
+
 .btn-check:checked + .btn, :not(.btn-check) + .btn:active, .btn:first-child:active, .btn.active, .btn.show{
   color: $low-black  ;
   background-color: lighten($brand-main, 15%);
   border-color: rgba($color: #000000, $alpha: 0)
 }
 }
+.map{
+  margin: auto;
+  width: 100%;
+  height: 270px;
+}
+
 
 </style>
