7ea6dcafb05fcc61686533f1643488b1a3c4c397
carloDM <carlo.de.mauro.work@gmail.com>
2023-07-27 19:46:55 +0200

    make copy input src

---
+++ resources/js/components/inputSearch copy.vue
@@ -0,0 +1,106 @@
+<script>
+import {store} from '../../data/store';
+import axios from 'axios';
+import {advancedSearch, convertAddress} from'../function/basicCall';
+export default {
+  name: 'InputSearch',
+  data(){
+    return{
+      store,
+      apiKey: store.apiKey,
+      indirizzo: '',
+      // --------------------------
+      autocomplete:'cerca',
+    }
+  },
+  methods :{
+
+    search(){
+
+      if(this.indirizzo.trim() == ''){
+        console.log('indirizzo vuoto');
+        this.indirizzo = 'Roma';
+      }
+
+      if(store.advSrcRequest.type === 'drv' && !store.advSrcRequest.coord){
+        console.warn('nessun poligono disegnato');
+
+      }else{
+
+        store.advSrcRequest.address = this.indirizzo;
+        console.log(store.TomtomBaseUrl + store.apiUrlSearchAddress + convertAddress(this.indirizzo) + store.queryType + this.apiKey);
+        // -------- chiamata centratura mappa
+        axios.get(store.TomtomBaseUrl + store.apiUrlSearchAddress + convertAddress(this.indirizzo) + store.queryType + store.apiKey)
+        .then(result =>{
+          store.mapCoord = [result.data.results[0].position.lon, result.data.results[0].position.lat];
+          console.log('store.mapCoord', store.mapCoord);
+          advancedSearch();
+          this.$router.push({ name: 'advancedSearch' });
+        })
+        .catch(function (error) {
+          console.warn(error);
+        })
+      }
+
+    },
+
+    updateAddress(){
+      if(store.lastRequest){
+      this.indirizzo =  store.lastRequest.address;
+    }
+    }
+  },
+
+  mounted(){
+
+    if(this.$route.name == 'home'){
+      this.indirizzo = ''
+    }else{
+      this.updateAddress();
+    }
+
+  }
+
+}
+</script>
+
+<template>
+    <div class="container g-1">
+
+        <div class="input-group g-0" id="" >
+          <input @keypress.enter="search()"
+          v-if="store.advSrcRequest.type === 'adv'"
+          id="via" v-model="indirizzo"
+          type="text"
+          class="form-control"
+          placeholder="Cerca dove vorresti andare"
+          >
+          <span @click="search()"
+          class="input-group-text search"
+          :class="{
+            'alone' : store.advSrcRequest.type === 'drv' || store.advSrcRequest.type === 'srv-only' ,
+            'deactivated' : store.advSrcRequest.type === 'drv' && !store.advSrcRequest.coord ,
+          }"
+          >
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </span>
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+@use '../../scss/var' as *;
+
+.alone{
+  width: 100%;
+  justify-content: center;
+
+}
+
+.deactivated{
+  opacity: 0.1;
+}
+
+</style>
